4c0a3261bbadd69fe6dd5dd684f421b57f463748
Lim Jiechao <jiechao.lim@gmail.com>
2017-10-09 11:00:01 +0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 var cards = ["Queen", "Queen", "King", "King"];
 var cardsInPlay = [];
-
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay[0]);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}
+// Check if the two flipped cards match each other. Provide feedback to the user letting them know if the two cards match, or if they should try again.
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		};
+	};
 }
+// Flipp over a card. When the user flips a card over, add that card to the array of cards that are in play. If the user has flipped over two cards, you'll want to check for a match.
+var flipCard = function(cardId) {
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+
+};
+
+flipCard(0);
+flipCard(2);
